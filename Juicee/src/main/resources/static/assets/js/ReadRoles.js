894fb42f321ad43967d7de8407213dd7d85c34b0
PetyrB2 <peter.i.broadbent@gmail.com>
2022-06-07 14:00:27 +0100

    Have added Read functionality with JavaScript. Not very pretty but it works. If I have the time I will come back to beautify the way the data we are retrieving from the database(SQL) is being displayed.

--- Juicee/src/main/resources/static/assets/js/ReadRoles.js
+++ Juicee/src/main/resources/static/assets/js/ReadRoles.js
@@ -1,8 +1,8 @@
-// A Javascript for POSTING User Registration Data back to the mySQL
+// A Javascript for GETTING Ingredients Data back to the mySQL
 
-// Function to GET CATEGORIES from the db
+// Function to GET INGREDIENTS from the db
 function fetchData() {
-    fetch('http://localhost:8080/Ingredients/read')
+    fetch('http://localhost:8080/Roles/read')
         .then((resp) => {
             if (resp.status != 200) console.error(resp.status);
             resp.json().then((data) => {
@@ -18,12 +18,18 @@ function showAllFacts(data) {
     for (let i = 0; i < data.length; i++) {
         const newDiv = document.createElement('div');
         const p = document.createElement('p');
-
-        p.append(document.createTextNode(`ID: ${data[i][`id`]} Name: ${data[i][`name`]}`));
-
+        const hr = document.createElement('hr');
+        
+        // String to beautify the data
+        p.append(document.createTextNode(`ID: ${data[i][`id`]}  `));
+        p.append(document.createTextNode(`Name: ${data[i][`name`]}  `));
+        // Append a p and a div for each field and new entry.
         newDiv.appendChild(p);
+        newDiv.appendChild(hr);
         rootElement.appendChild(newDiv);
     }
 }
 fetchData();
 // showAllFacts();
+
+
--- Juicee/src/main/resources/static/assets/js/ReadUsers.js
+++ Juicee/src/main/resources/static/assets/js/ReadUsers.js
@@ -1,51 +1,29 @@
-// A Javascript file to GET User Data
-// ==============================================
-// New Function to FETCH USERS from the db
+// A Javascript for GETTING Ingredients Data back to the mySQL
 
+// Function to GET INGREDIENTS from the db
 function fetchData() {
     fetch('http://localhost:8080/Users/read')
-
-    .then((resp) => resp.json())
-    .then((data) => (data))
+        .then((resp) => {
+            if (resp.status != 200) console.error(resp.status);
+            resp.json().then((data) => {
+                console.log(data)
+                showAllFacts(data)
+            })
+        });
 }
 
-// A Javascript for showing the User Data
-
-function showData(data) {
-    const rootElement = document.getElementsByClass('root')[0];
-    for (let fact of data) {
+function showAllFacts(data) {
+    console.table(data)
+    const rootElement = document.getElementsByClassName('root')[0];
+    for (let i = 0; i < data.length; i++) {
         const newDiv = document.createElement('div');
         const p = document.createElement('p');
-
-        p.append(document.createTextNode(fact.fact));
+        // String to beautify the data
+        p.append(document.createTextNode(`ID: ${data[i][`id`]} Email: ${data[i][`email`]} Password: ${data[i][`password`]} First Name: ${data[i][`firstName`]} Last Name: ${data[i][`lastName`]}`));
+        // Append a p and a div for each field and new entry.
         newDiv.appendChild(p);
         rootElement.appendChild(newDiv);
     }
 }
-const button = document.querySelector('submit');
-
-button.addEventListener('click', () = > {
-    showData();
-})
-;
 fetchData();
-showData();
-
-
-// ==============================================
-// Add a Delete function with checkboxes next to Users if possible.
-// ==============================================
-// function deleteData(data) {
-//     let id = 1; // change to a select the ID
-//     fetch("http://localhost:8080/Users/read/"+id, {  //2
-//         method: 'delete'  // method 
-//       })
-//       .then((data) => {
-//         console.log(`Request succeeded with JSON response ${data}`);
-//        // refresh page and display that delete has completed
-//       })
-//       .catch((error) => {
-//         //some function to execute if error
-//       });
-
-// }
+// showAllFacts();
